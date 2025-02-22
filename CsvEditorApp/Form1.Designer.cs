7f46c49a53101945dfbf5ccf1b400004354dd827
MasayaTma <hedaozhenye@gmail.com>
2025-02-23 00:09:21 +0900

    プロジェクト ファイルを追加します。

---
+++ CsvEditorApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CsvEditorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.dgvCsv = new System.Windows.Forms.DataGridView();
+            this.btnLoadCsv = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnSaveCsv = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCsv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.Location = new System.Drawing.Point(0, 23);
+            this.txtEditor.Margin = new System.Windows.Forms.Padding(0);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(888, 509);
+            this.txtEditor.TabIndex = 2;
+            this.txtEditor.TextChanged += new System.EventHandler(this.txtEditor_TextChanged);
+            // 
+            // dgvCsv
+            // 
+            this.dgvCsv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCsv.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvCsv.Location = new System.Drawing.Point(0, 78);
+            this.dgvCsv.Name = "dgvCsv";
+            this.dgvCsv.RowTemplate.Height = 21;
+            this.dgvCsv.Size = new System.Drawing.Size(888, 454);
+            this.dgvCsv.TabIndex = 3;
+            // 
+            // btnLoadCsv
+            // 
+            this.btnLoadCsv.Location = new System.Drawing.Point(0, 2);
+            this.btnLoadCsv.Name = "btnLoadCsv";
+            this.btnLoadCsv.Size = new System.Drawing.Size(105, 23);
+            this.btnLoadCsv.TabIndex = 4;
+            this.btnLoadCsv.Text = "CSV読込";
+            this.btnLoadCsv.UseVisualStyleBackColor = true;
+            this.btnLoadCsv.Click += new System.EventHandler(this.btnLoadCsv_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(111, 2);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 5;
+            this.btnDisplay.Text = "表示";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnSaveCsv
+            // 
+            this.btnSaveCsv.Location = new System.Drawing.Point(192, 2);
+            this.btnSaveCsv.Name = "btnSaveCsv";
+            this.btnSaveCsv.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveCsv.TabIndex = 6;
+            this.btnSaveCsv.Text = "保存";
+            this.btnSaveCsv.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 532);
+            this.Controls.Add(this.btnSaveCsv);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnLoadCsv);
+            this.Controls.Add(this.dgvCsv);
+            this.Controls.Add(this.txtEditor);
+            this.Name = "Form1";
+            this.Text = "CSV Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCsv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtEditor;
+        private System.Windows.Forms.DataGridView dgvCsv;
+        private System.Windows.Forms.Button btnLoadCsv;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnSaveCsv;
+    }
+}
---
+++ CsvEditorApp/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsvEditorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // CSVファイルを読み込むボタン
+        private async void btnLoadCsv_Click(object sender, EventArgs e)
+        {
+            // ファイル選択ダイアログを開く
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                // 非同期でCSVの内容を読み込み
+                string fileContent = await Task.Run(() => File.ReadAllText(filePath));
+
+                // 読み込んだ内容をテキストボックスに表示
+                txtEditor.Text = fileContent;
+
+                // CSVデータをDataGridViewに表示
+                await Task.Run(() => DisplayCsvData(fileContent));
+            }
+        }
+
+        // 表示ボタン: TextBoxの内容をカンマ区切りでDataGridViewに表示
+        private async void btnDisplay_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => DisplayCsvData(txtEditor.Text));
+        }
+
+        // CSVデータをDataGridViewに表示する共通メソッド
+        private void DisplayCsvData(string csvContent)
+        {
+            string[] lines = csvContent.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            DataGridView dataGridView = dgvCsv;
+
+            // UIスレッドをブロックしないようにする
+            dataGridView.Invoke(new Action(() =>
+            {
+                dataGridView.Rows.Clear();
+                dataGridView.Columns.Clear();
+
+                // 列の追加（最初の行にカラム名を設定）
+                if (lines.Length > 0)
+                {
+                    string[] columns = lines[0].Split(',');
+
+                    foreach (var column in columns)
+                    {
+                        dataGridView.Columns.Add(column.Trim(), column.Trim());
+                    }
+
+                    // 行の追加（最初の行はヘッダーとして、2行目以降をデータとして追加）
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        string[] rowValues = lines[i].Split(',');
+
+                        // 空白行は無視
+                        if (rowValues.Length == 0 || string.IsNullOrWhiteSpace(string.Join("", rowValues)))
+                        {
+                            continue;
+                        }
+
+                        dataGridView.Rows.Add(rowValues);
+                    }
+                }
+
+                // 列幅の自動調整
+                foreach (DataGridViewColumn col in dataGridView.Columns)
+                {
+                    col.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                }
+            }));
+        }
+
+        // テキストボックスに選択したセルの値を表示
+        private void dgvCsv_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvCsv.SelectedCells.Count > 0)
+            {
+                txtEditor.Text = dgvCsv.SelectedCells[0].Value?.ToString();
+            }
+        }
+
+        // テキストボックスの内容が変更されたときにDataGridViewのセルを更新
+        private void txtEditor_TextChanged(object sender, EventArgs e)
+        {
+            if (dgvCsv.SelectedCells.Count > 0)
+            {
+                dgvCsv.SelectedCells[0].Value = txtEditor.Text;
+            }
+        }
+
+        // 変更内容をCSVファイルとして保存するボタン
+        private void btnSaveCsv_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                SaveCsvData(filePath);
+            }
+        }
+
+        // DataGridViewの内容をCSV形式で保存
+        private void SaveCsvData(string filePath)
+        {
+            StringBuilder csvContent = new StringBuilder();
+
+            // ヘッダー行を追加
+            for (int i = 0; i < dgvCsv.Columns.Count; i++)
+            {
+                csvContent.Append(dgvCsv.Columns[i].HeaderText);
+                if (i < dgvCsv.Columns.Count - 1) csvContent.Append(",");
+            }
+            csvContent.AppendLine();
+
+            // データ行を追加
+            foreach (DataGridViewRow row in dgvCsv.Rows)
+            {
+                for (int i = 0; i < dgvCsv.Columns.Count; i++)
+                {
+                    csvContent.Append(row.Cells[i].Value?.ToString());
+                    if (i < dgvCsv.Columns.Count - 1) csvContent.Append(",");
+                }
+                csvContent.AppendLine();
+            }
+
+            // ファイルに書き込む
+            File.WriteAllText(filePath, csvContent.ToString());
+        }
+
+        // フォーム読み込み時に設定する処理
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvCsv.SelectionChanged += dgvCsv_SelectionChanged;
+        }
+    }
+}
+　
